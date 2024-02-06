2fd3192e736a51db78180457ab0e743edac409c4
Bhargav2525 <bhargav.katabathuni@antstack.io>
2024-02-06 12:49:09 +0530

    app.py changed

--- .aws-sam/build/HelloWorldFunction/app.py
+++ .aws-sam/build/HelloWorldFunction/app.py
@@ -1,5 +1,4 @@
 import json
-import requests
 import boto3
 import os
 from PIL import Image
@@ -14,8 +13,8 @@ table_name = 'thumbnail-app-MetaTable-UT3OEO35G2OF'
 table = dynamodb.Table(table_name)
 events = boto3.client('events')
 
+# Handles Metadata of the image and inserts into DDB.
 def metadata_handler(event, context):
-    print(event)
     detail_data = event['detail']
     filename = detail_data['filename']
     Time = detail_data['Time']
@@ -24,25 +23,32 @@ def metadata_handler(event, context):
     item = {"filename": filename ,"Time" : Time, "original_url": original_url , "thumbnail_url" : thumbnail_url}
     table.put_item(Item = item)
     return {}
+    # while using step function
+    # table.put_item(Item = event)
+    # return {}
+
+# Generates Thumbnail version of the image using PIL and inserts into S3.
 def thumbnail_generator(event,context):
-    file_insertion_time = event["Records"][0]["eventTime"]
+    print(event)
     try:
         file_name = event["Records"][0]['s3']['object']['key']
-        response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
+        file_insertion_time = event["Records"][0]["eventTime"]
+        response = s3_client.get_object(Bucket=bucket_name, Key= file_name)
         image_data = response['Body'].read()
         image = Image.open(BytesIO(image_data))
-        thumbnail_file_name = file_name + '_thumbnail.jpg'
+        thumbnail_file_name = "thumbnail/" + file_name.split("/")[1]
         thumbnail_image = image.resize((100, 100))
         buffered_resized = BytesIO()
         thumbnail_image.save(buffered_resized, format="JPEG")  
         thumbnail_image_data = buffered_resized.getvalue()
         s3_client.put_object(
-            Bucket=second_bucket_name,
-            Key=thumbnail_file_name,
-            Body=thumbnail_image_data,
+            Bucket=bucket_name,
+            Key = thumbnail_file_name,
+            Body = thumbnail_image_data,
             ContentType="image/jpeg"
         )
     except Exception as e:
+        print(e)
         print("Exception in File Insertion Block")
         return {
             "StatusCode" : e.args[0],
@@ -60,11 +66,10 @@ def thumbnail_generator(event,context):
             },
             ExpiresIn=360000
         )
-
         url2 = s3_client.generate_presigned_url(
             ClientMethod='get_object',
             Params={
-                'Bucket':second_bucket_name,
+                'Bucket': bucket_name ,
                 'Key': thumbnail_file_name,
                 'ResponseContentType': 'text/plain',
                 'ResponseContentDisposition': 'attachment'
@@ -75,7 +80,7 @@ def thumbnail_generator(event,context):
     except Exception as e:
         print("Error in Generating Presigned URL")
     
-    item = {"filename" : file_name,"Time":str(file_insertion_time), "original_url": url1 ,"thumbnail_url":url2}
+    item = {"filename" : file_name.split("/")[1],"Time":str(file_insertion_time), "original_url": url1 ,"thumbnail_url": url2}
     response = events.put_events(
         Entries=[
             {
@@ -92,8 +97,63 @@ def thumbnail_generator(event,context):
     )
     print(response)
     return response
+    # while using step function
+    # try:
+    #     print(event)
+    #     file_name = event["detail"]['object']['key']
+    #     file_insertion_time = event["time"]
+    #     response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
+    #     image_data = response['Body'].read()
+    #     image = Image.open(BytesIO(image_data))
+    #     thumbnail_file_name = file_name + '_thumbnail.png'
+    #     thumbnail_image = image.resize((100, 100))
+    #     buffered_resized = BytesIO()
+    #     thumbnail_image.save(buffered_resized, format="JPEG")  
+    #     thumbnail_image_data = buffered_resized.getvalue()
+    #     s3_client.put_object(
+    #         Bucket=second_bucket_name,
+    #         Key=thumbnail_file_name,
+    #         Body=thumbnail_image_data,
+    #         ContentType="image/jpeg" 
+    #     )
+    # except Exception as e:
+    #     print("Exception in File Insertion Block")
+    #     print(e)
+    #     return {
+    #         "StatusCode" : e.args[0],
+    #         "headers" : {},
+    #         "message" : e.args[1]
+    #     }
+    # try:
+    #     url1 = s3_client.generate_presigned_url(
+    #         ClientMethod='get_object',
+    #         Params={
+    #             'Bucket': bucket_name,
+    #             'Key': file_name,
+    #             'ResponseContentType': 'text/plain',
+    #             'ResponseContentDisposition': 'attachment'
+    #         },
+    #         ExpiresIn=360000
+    #     )
+    #     url2 = s3_client.generate_presigned_url(
+    #         ClientMethod='get_object',
+    #         Params={
+    #             'Bucket':second_bucket_name,
+    #             'Key': thumbnail_file_name,
+    #             'ResponseContentType': 'text/plain',
+    #             'ResponseContentDisposition': 'attachment'
+    #         },
+    #         ExpiresIn=360000
+    #     )
+    #     print("url1:",url1,"url2:",url2)
+    # except Exception as e:
+    #     print("Error in Generating Presigned URL")
+    
+    # item = {"filename" : file_name,"Time":str(file_insertion_time), "original_url": url1 ,"thumbnail_url": url2}
+    # return item
+    
 
-
+# Generates Presigned Post URL to upload files into S3.
 def lambda_handler(event, context):
     bucket_name = os.environ.get("bucket_name", None)
     try:
@@ -111,5 +171,5 @@ def lambda_handler(event, context):
         }
     except Exception as e:
         return {"statusCode": e.args[0], "body": json.dumps("Error in Exception block of Lambda handler")}
-
+    
     return {"statusCode": 500, "body": json.dumps("Error processing the request!")}
--- hello_world/app.py
+++ hello_world/app.py
@@ -1,5 +1,4 @@
 import json
-import requests
 import boto3
 import os
 from PIL import Image
@@ -16,7 +15,6 @@ events = boto3.client('events')
 
 # Handles Metadata of the image and inserts into DDB.
 def metadata_handler(event, context):
-    print(event)
     detail_data = event['detail']
     filename = detail_data['filename']
     Time = detail_data['Time']
@@ -25,27 +23,32 @@ def metadata_handler(event, context):
     item = {"filename": filename ,"Time" : Time, "original_url": original_url , "thumbnail_url" : thumbnail_url}
     table.put_item(Item = item)
     return {}
+    # while using step function
+    # table.put_item(Item = event)
+    # return {}
 
 # Generates Thumbnail version of the image using PIL and inserts into S3.
 def thumbnail_generator(event,context):
+    print(event)
     try:
         file_name = event["Records"][0]['s3']['object']['key']
         file_insertion_time = event["Records"][0]["eventTime"]
-        response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
+        response = s3_client.get_object(Bucket=bucket_name, Key= file_name)
         image_data = response['Body'].read()
         image = Image.open(BytesIO(image_data))
-        thumbnail_file_name = file_name + '_thumbnail.jpg'
+        thumbnail_file_name = "thumbnail/" + file_name.split("/")[1]
         thumbnail_image = image.resize((100, 100))
         buffered_resized = BytesIO()
         thumbnail_image.save(buffered_resized, format="JPEG")  
         thumbnail_image_data = buffered_resized.getvalue()
         s3_client.put_object(
-            Bucket=second_bucket_name,
-            Key=thumbnail_file_name,
-            Body=thumbnail_image_data,
+            Bucket=bucket_name,
+            Key = thumbnail_file_name,
+            Body = thumbnail_image_data,
             ContentType="image/jpeg"
         )
     except Exception as e:
+        print(e)
         print("Exception in File Insertion Block")
         return {
             "StatusCode" : e.args[0],
@@ -66,7 +69,7 @@ def thumbnail_generator(event,context):
         url2 = s3_client.generate_presigned_url(
             ClientMethod='get_object',
             Params={
-                'Bucket':second_bucket_name,
+                'Bucket': bucket_name ,
                 'Key': thumbnail_file_name,
                 'ResponseContentType': 'text/plain',
                 'ResponseContentDisposition': 'attachment'
@@ -77,7 +80,7 @@ def thumbnail_generator(event,context):
     except Exception as e:
         print("Error in Generating Presigned URL")
     
-    item = {"filename" : file_name,"Time":str(file_insertion_time), "original_url": url1 ,"thumbnail_url": url2}
+    item = {"filename" : file_name.split("/")[1],"Time":str(file_insertion_time), "original_url": url1 ,"thumbnail_url": url2}
     response = events.put_events(
         Entries=[
             {
@@ -94,6 +97,61 @@ def thumbnail_generator(event,context):
     )
     print(response)
     return response
+    # while using step function
+    # try:
+    #     print(event)
+    #     file_name = event["detail"]['object']['key']
+    #     file_insertion_time = event["time"]
+    #     response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
+    #     image_data = response['Body'].read()
+    #     image = Image.open(BytesIO(image_data))
+    #     thumbnail_file_name = file_name + '_thumbnail.png'
+    #     thumbnail_image = image.resize((100, 100))
+    #     buffered_resized = BytesIO()
+    #     thumbnail_image.save(buffered_resized, format="JPEG")  
+    #     thumbnail_image_data = buffered_resized.getvalue()
+    #     s3_client.put_object(
+    #         Bucket=second_bucket_name,
+    #         Key=thumbnail_file_name,
+    #         Body=thumbnail_image_data,
+    #         ContentType="image/jpeg" 
+    #     )
+    # except Exception as e:
+    #     print("Exception in File Insertion Block")
+    #     print(e)
+    #     return {
+    #         "StatusCode" : e.args[0],
+    #         "headers" : {},
+    #         "message" : e.args[1]
+    #     }
+    # try:
+    #     url1 = s3_client.generate_presigned_url(
+    #         ClientMethod='get_object',
+    #         Params={
+    #             'Bucket': bucket_name,
+    #             'Key': file_name,
+    #             'ResponseContentType': 'text/plain',
+    #             'ResponseContentDisposition': 'attachment'
+    #         },
+    #         ExpiresIn=360000
+    #     )
+    #     url2 = s3_client.generate_presigned_url(
+    #         ClientMethod='get_object',
+    #         Params={
+    #             'Bucket':second_bucket_name,
+    #             'Key': thumbnail_file_name,
+    #             'ResponseContentType': 'text/plain',
+    #             'ResponseContentDisposition': 'attachment'
+    #         },
+    #         ExpiresIn=360000
+    #     )
+    #     print("url1:",url1,"url2:",url2)
+    # except Exception as e:
+    #     print("Error in Generating Presigned URL")
+    
+    # item = {"filename" : file_name,"Time":str(file_insertion_time), "original_url": url1 ,"thumbnail_url": url2}
+    # return item
+    
 
 # Generates Presigned Post URL to upload files into S3.
 def lambda_handler(event, context):
